6ffed7101562fa6a73b4e5f474ce0801cd5f2ec8
choibits <152449776+choibits@users.noreply.github.com>
2024-04-12 10:32:33 -0700

    Add files via upload

---
+++ knitcounter.js
@@ -0,0 +1,25 @@
+
+
+
+
+ //function that resets the row count when you hit the reset button
+ document.querySelector('.row-counter-reset').addEventListener('click', function() {
+    document.querySelector('.row-counter-display').innerHTML= 0;
+});
+
+//call value and set innerhtml of the row count equal to this number
+document.querySelector('#rowButton').addEventListener('click', function() {
+    document.querySelector('.row-counter-display').innerHTML = document.querySelector('#rows').value;
+});
+
+// function that lowers the rows left by 1 when you hit the plus button
+document.querySelector('.row-counter-plus').onclick = function() {
+    if ((document.querySelector('.row-counter-display').innerHTML) > 0){
+        (document.querySelector('.row-counter-display').innerHTML)--;
+    }
+ };
+
+ // function that adds the rows left by 1 when you hit the minus button
+document.querySelector('.row-counter-minus').onclick = function() {
+    (document.querySelector('.row-counter-display').innerHTML)++;
+ };
