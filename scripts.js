780f967cc2ec64059938aa5c8e463d4907156030
choibits <152449776+choibits@users.noreply.github.com>
2024-08-16 16:20:05 -0400

    Overhaul and rename knitcounter.js to scripts.js

---
+++ scripts.js
@@ -0,0 +1,32 @@
+const titleDisplay = document.querySelector('#title');
+const countdownDisplay = document.querySelector('#countdown')
+let displayValue = 0;
+let countdownValue = 0;
+
+const minusButton = document.querySelector('#minus');
+const resetButton = document.querySelector('#reset');
+const plusButton = document.querySelector('#plus');
+const submitButton = document.querySelector('#submit-button');
+
+plusButton.addEventListener('click', () => {
+    displayValue++;
+    titleDisplay.textContent = displayValue;
+    countdownDisplay.textContent--;
+});
+
+minusButton.addEventListener('click', () => {
+    if (displayValue > 0) {
+        displayValue--;
+    }
+    titleDisplay.textContent = displayValue;
+    countdownDisplay.textContent++;
+});
+
+resetButton.addEventListener('click', () => {
+    displayValue = 0;
+    titleDisplay.textContent = displayValue;
+});
+
+submitButton.addEventListener('click', () => {
+    countdownDisplay.textContent = document.querySelector('#num').value;
+});
